4eda4c03c3e9c3103b9d25dbb9092ed9717d982c
SavageCabbage64 <jake.berryman@curreyingram.org>
2025-03-02 22:29:38 -0600

    add configuration files and update existing settings

--- kubejs/server_scripts/tweaks.js
+++ kubejs/server_scripts/tweaks.js
@@ -1,3 +1,21 @@
 ServerEvents.recipes(event => {
+    event.shaped(
+        '3x minecraft:ladder',
+        [
+          'S S',
+          'SSS', 
+          'S S'
+        ],
+        {
+            S: '#forge:rods/wooden'
+        }
+    ).id('minecraft:ladder');
 
+    //replace culture squid with miner squid
+    event.replaceInput({}, 'culturaldelights:squid','miners_delight:squid');
+    event.replaceInput({}, 'culturaldelights:glow_squid','miners_delight:glow_squid');
+    event.replaceInput({}, 'culturaldelights:raw_calamari','miners_delight:tentacles');
+    event.remove({output: 'culturaldelights:cooked_squid'});
+    event.remove({output: 'culturaldelights:raw_calamari'});
+    event.remove({output: 'culturaldelights:cooked_calamari'});
 })
