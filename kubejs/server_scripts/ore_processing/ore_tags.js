1c8e058bbf64b333e4bf7c597852be17cefb6d8f
Jake Berryman <dragons4me@comcast.net>
2025-09-03 13:15:20 -0500

    add new ore types

---
+++ kubejs/server_scripts/ore_processing/ore_tags.js
@@ -0,0 +1,7 @@
+ServerEvents.tags("item", (event) => {
+    // ORE_STONE_TYPES.forEach(stone => {
+    //     ORE_MINERAL_TYPES.forEach(ore => {
+    //         event.add(`${stone[0]}_${ore[0]}_ore` ,`forge:ores/${ore}`)
+    //     })
+    // })
+})
--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -1,21 +1,97 @@
+const ORE_MINERAL_TYPES = [
+  ["aluminum", "immersiveengineering:ore_aluminum"],
+  ["lead", "immersiveengineering:ore_lead"],
+  ["nickel", "immersiveengineering:ore_nickel"],
+  ["silver", "immersiveengineering:ore_silver"],
+  ["uranium", "mekanism:uranium_ore"],
+  ["osmium", "mekanism:osmium_ore"],
+  ["tin", "mekanism:tin_ore"],
+  ["fluorite", "mekanism:fluorite_ore"],
+  // Vanilla ores
+  ["coal", "minecraft:coal_ore"],
+  ["iron", "minecraft:iron_ore"],
+  ["gold", "minecraft:gold_ore"],
+  ["diamond", "minecraft:diamond_ore"],
+  ["emerald", "minecraft:emerald_ore"],
+  ["copper", "minecraft:copper_ore"],
+  ["lapis", "minecraft:lapis_ore"],
+  ["quartz", "minecraft:nether_quartz_ore"]
+]
+// add andesite, granite, type ores later
+const ORE_STONE_TYPES = [
+  ["andesite", "minecraft:andesite"],
+  ["granite", "minecraft:granite"],
+  ["diorite", "minecraft:diorite"],
+  ["tuff", "minecraft:tuff"],
+  ["nether", "minecraft:netherrack"],
+  ["end", "minecraft:end_stone"],
+  ["moon", "ad_astra:moon_stone"],
+  ["mars", "ad_astra:mars_stone"],
+  ["venus", "ad_astra:venus_stone"],
+  ["mercury", "ad_astra:mercury_stone"],
+  ["glacio", "ad_astra:glacio_stone"]
+]
+
+const $BlockBehaviour$Properties = Java.loadClass('net.minecraft.world.level.block.state.BlockBehaviour$Properties');
+const $RedStoneOreBlock = Java.loadClass("net.minecraft.world.level.block.RedStoneOreBlock")
+const $Item$Properties = Java.loadClass("net.minecraft.world.item.Item$Properties");
+const $BlockItem = Java.loadClass("net.minecraft.world.item.BlockItem")
+const $Gas = Java.loadClass('mekanism.api.chemical.gas.Gas')
+const $GasBuilder = Java.loadClass('mekanism.api.chemical.gas.GasBuilder')
+// const $Slurry = Java.loadClass('mekanism.api.chemical.slurry.Slurry')
+// const $SlurryBuilder = Java.loadClass('mekanism.api.chemical.slurry.SlurryBuilder')
+
+let redstoneOres = []
+
 StartupEvents.registry("block", (event) => {
-  event
-    .create("limesand", "falling")
-    .tagBlock("minecraft:mineable/shovel")
-    .hardness(0.5)
-    .resistance(0.5)
-    .sandSoundType()
-    .mapColor("quartz");
-  event
-    .create("scoria_aggregate", "falling")
-    .tagBlock("minecraft:mineable/shovel")
-    .hardness(0.6)
-    .resistance(0.6)
-    .gravelSoundType()
-    .mapColor("terracotta_black");
+  ORE_STONE_TYPES.forEach(stone => {
+    //var block = Block.of(stone[1])
+    ORE_MINERAL_TYPES.forEach(ore => {
+      event
+        .create(`${stone[0]}_${ore[0]}_ore`)
+        .tagBlock("minecraft:mineable/pickaxe")
+        .hardness(1.5)
+        .resistance(1.5)
+        .requiresTool(true)
+        .textureAll(`ores/${ore[0]}/${stone[0]}_${ore[0]}_ore`)
+      // .setModelJson(value: json)
+      // .sandSoundType()
+      // .mapColor("stone");
+    })
+
+
+    let redstoneOreBlock = event.createCustom(`${stone[0]}_redstone_ore`, () => {
+      const baseProperties = $BlockBehaviour$Properties.copy(Blocks.REDSTONE_ORE);
+      return new $RedStoneOreBlock(baseProperties);
+    })
+
+    redstoneOres.push(redstoneOreBlock)
+  })
+
+event
+  .create("limesand", "falling")
+  .tagBlock("minecraft:mineable/shovel")
+  .hardness(0.5)
+  .resistance(0.5)
+  .sandSoundType()
+  .mapColor("quartz");
+event
+  .create("scoria_aggregate", "falling")
+  .tagBlock("minecraft:mineable/shovel")
+  .hardness(0.6)
+  .resistance(0.6)
+  .gravelSoundType()
+  .mapColor("terracotta_black");
 });
 
 StartupEvents.registry("item", (event) => {
+  
+  let i = 0
+  redstoneOres.forEach(redstoneOre => {
+    event.createCustom(`${ORE_STONE_TYPES[i][0]}_redstone_ore`, () => $BlockItem(redstoneOre.get(), new $Item$Properties()));
+    i++
+  })
+
   event.create("incomplete_logic_processor", "create:sequenced_assembly");
   event.create("incomplete_calculation_processor", "create:sequenced_assembly");
   event.create("incomplete_engineering_processor", "create:sequenced_assembly");
@@ -23,7 +99,7 @@ StartupEvents.registry("item", (event) => {
   event
     .create("netherite_fluix_compound")
     .displayName("Netherite-Fluix Compound");
-  
+
   event.create("energized_alloy").glow(true);
 
   event
@@ -58,7 +134,7 @@ StartupEvents.registry("fluid", (event) => {
     .create("polonium_fusion_fuel")
     .displayName("Liquid Polonium-Enhanced D-T Compound")
 
-    event
+  event
     .create("rocket_fuel_1")
     .stillTexture("kubejs:block/fuel_still")
     .flowingTexture("kubejs:block/fuel_flow")
@@ -116,12 +192,6 @@ StartupEvents.registry("fluid", (event) => {
     .bucketItem
     .tooltip("§7Efficient fuel for a Tier 4 Rocket and below")
 })
-
-// const $Slurry = Java.loadClass('mekanism.api.chemical.slurry.Slurry')
-// const $SlurryBuilder = Java.loadClass('mekanism.api.chemical.slurry.SlurryBuilder')
-
-const $Gas = Java.loadClass('mekanism.api.chemical.gas.Gas')
-const $GasBuilder = Java.loadClass('mekanism.api.chemical.gas.GasBuilder')
 
 StartupEvents.registry('mekanism:gas', event => {
   event.createCustom('chloroethane', () => $Gas($GasBuilder.builder()))
